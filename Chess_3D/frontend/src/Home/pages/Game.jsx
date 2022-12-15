44236d395cf2794b60cd28af23ef9949473d1b7b
Uli Rodriguez <95945011+UlizesR@users.noreply.github.com>
2022-12-14 20:43:26 -0800

    Delete Chess_3D directory

--- Chess_3D/frontend/src/Home/pages/Game.jsx
+++
@@ -1,9 +0,0 @@
-import GameScene from "../../game/scene/GameScene";
-
-const Game = () => {
-    return (
-        <GameScene />
-    )
-}
-
-export default Game;
--- Chess_3D/frontend/src/game/utils/utils.js
+++
@@ -1,7 +0,0 @@
-export const arrayEqual = ([a], [b]) => {
-    if (a.length !== b.length) {
-        return false;
-    }
-
-    return [a].every((_, i) => a[i] === b[i]);
-};
