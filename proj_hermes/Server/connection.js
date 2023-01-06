2210699ea20dd5c771520ff15df5bba72d176b7f
Uli <fulizesraudales@gmail.com>
2023-01-05 18:01:04 -0800

    removed hermes folder

--- proj_hermes/Server/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb+srv://db-admin:${process.env.DB_PASSWORD}>@hermeschat-db.5qlqi0m.mongodb.net/?retryWrites=true&w=majority`, () => {
-    console.log('connected to mongodb')
-})
